b38313b6cef08cc3161fa992f2744b1e5fbbb4b8
Ivan Szebenszki <szib77@gmail.com>
2019-04-29 10:05:57 +0100

    Done.
    Add Sea model, migration
    
    Add SeasController
    
    Fix bg-color
    
    Add seed
    
    Add .row

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,13 +1,63 @@
+
 class SeasController < ApplicationController
-  #define your controller actions here
-
-
-  private
-  # In controller actions, use this private method to access sea params from forms.
-  # Example: @sea.update(sea_params)
-  # check the return value of sea_params to see what you're working with!
-  def sea_params
-    params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
+  before_action :find_sea, except: [:index, :new, :create]
+  
+  def index
+    @seas = Sea.all
   end
 
+  def show
+  end
+  
+  def new
+    @sea = Sea.new
+  end
+  
+  def edit
+  end
+
+  def create
+    @sea = Sea.new(sea_params)
+    if @sea.save
+      flash[:success] = "Sea successfully created"
+      redirect_to @sea
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  def update
+    if @sea.update_attributes(sea_params)
+      flash[:success] = "Sea was successfully updated"
+      redirect_to @sea
+    else
+      flash[:error] = "Something went wrong"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    if @sea.destroy
+      flash[:success] = "Sea was successfully deleted"
+      redirect_to @seas_path
+    else
+      flash[:error] = "Something went wrong"
+      redirect_to @seas_path
+    end
+  end
+  
+  private
+    # In controller actions, use this private method to access sea params from forms.
+    # Example: @sea.update(sea_params)
+    # check the return value of sea_params to see what you're working with!
+    def sea_params
+      params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
+    end
+
+    def find_sea
+      @sea = Sea.find(params[:id])
+    end
+    
 end
+  
